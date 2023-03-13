f4f2125879bf2da9ee3b6542dc0d8ba7eb584df4
Pelayo <phuermij@myuax.com>
2023-03-13 11:55:50 +0100

    funcion lanzador

--- lanzador.py
+++ lanzador.py
@@ -1,5 +1,15 @@
 from Filosofos import *
 
 def lanzador():
+    tenedores = []
+    for i in range(5):
+        tenedores.append(threading.Lock())
     filosofos = []
     nombre_filosofos = ["Aristóteles", "Sócrates", "Kant", "Platón", "Pitágoras"]
+
+    for i in range(5):
+        filosofo = Filosofos(nombre_filosofos[i], tenedores[i], tenedores[i+1])
+        filosofos.append(filosofo)
+
+    for filosofo in filosofos:
+        filosofo.start()

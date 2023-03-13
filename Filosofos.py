6f4ca6bfd6cc6487e678cd367c3e193af6b7dd17
Pelayo <phuermij@myuax.com>
2023-03-13 11:26:28 +0100

    mas de funcion comer

--- Filosofos.py
+++ Filosofos.py
@@ -1,4 +1,6 @@
 import threading
+import time
+
 
 class Filosofos(threading.Thread):
     def __init__(self, nombre, tenedorIzq, tenedorDcho):
@@ -11,10 +13,18 @@ class Filosofos(threading.Thread):
         print(self.nombre, "está pensando.")
 
     def comer(self):
-        print(self.nombre, "está comiendo.")
+        self.tenedorDcho.acquire()
+        print(self.nombre, "cogió el tenedor derecho")
+        self.tenedorIzq.acquire()
+        print(self.nombre, "cogió el tenedor izquierdo")
+        print(self.nombre, "tiene dos tenedores y puede comer.")
+        time.sleep()
     
     def dormir(self):
         print(self.nombre, "está durmiendo.")
-        
+
+    def iniciar(self):
+
+
     
 

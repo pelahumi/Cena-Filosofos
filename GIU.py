ae9582c2f40a4842b373c2363b68d3c3321fcf98
Pelayo <phuermij@myuax.com>
2023-05-08 14:25:37 +0200

    interfaz

--- GIU.py
+++ GIU.py
@@ -1,34 +1,11 @@
 import tkinter
+import sys
 from Lanzador import lanzador
 
 ventana = tkinter.Tk()
 ventana.title("Cena de los Filósofos")
-ventana.geometry("500x500")
-ventana.config(bg="white")
 
-boton = tkinter.Button(ventana, text="Ejecutar", command=lanzador)
-boton.place(x=100, y=350)
-
-canvas1 = tkinter.Canvas(ventana, width=100, height=50, background="pink")
-canvas1.create_text(150, 75, text="Pitágoras")
-canvas1.place(x=100, y=50)
-
-canvas2 = tkinter.Canvas(ventana, width=100, height=50, background="orange")
-canvas2.create_text(75, 150, text="Kant")
-canvas2.place(x=25,y=125)
-
-canvas3 = tkinter.Canvas(ventana, width=100, height=50, background="pink")
-canvas3.create_text(100, 225, text="Aristóteles")
-canvas3.place(x=50, y=200)
-
-canvas4 = tkinter.Canvas(ventana, width=100, height=50, background="orange")
-canvas4.create_text(225, 200, text="Platón")
-canvas4.place(x=275, y=175)
-
-canvas5 = tkinter.Canvas(ventana, width=100, height=50, background="pink")
-canvas5.create_text(250, 135, text="Sócrates")
-canvas5.place(x=200, y=160)
-"""
+canvas = tkinter.Canvas(bg="white", height=500, width=500)
 
 canvas.create_rectangle(100, 50, 200, 100, fill="pink")
 canvas.create_rectangle(25, 125, 125, 175, fill="orange")
@@ -42,7 +19,29 @@ canvas.create_text(100, 225, text="Aristóteles")
 canvas.create_text(225, 200, text="Platón")
 canvas.create_text(250, 135, text="Sócrates")
 
+canvas.create_rectangle(60, 80, 80, 100, fill="dark blue")
+canvas.create_rectangle(20, 200, 40, 220, fill="dark blue")
+canvas.create_rectangle(220, 80, 240, 100, fill="dark blue")
+canvas.create_rectangle(290, 180, 310, 200, fill="dark blue")
+canvas.create_rectangle(170, 240, 190, 260, fill="dark blue")
+
+canvas.create_text(70, 90, text="1", fill="white")
+canvas.create_text(30, 210, text="2", fill="white")
+canvas.create_text(230, 90, text="5", fill="white")
+canvas.create_text(300, 190, text="4", fill="white")
+canvas.create_text(180, 250, text="3",fill="white")
+
 canvas.pack()
-"""
+
+boton_ejecutar = tkinter.Button(canvas, text="Ejecutar", command=lanzador)
+boton_ejecutar.place(x=100, y=350)
+
+boton_cerrar = tkinter.Button(canvas, text="Salir", command=sys.exit)
+boton_cerrar.place(x=200, y= 350)
+
+
+
+
+
 
 ventana.mainloop()
